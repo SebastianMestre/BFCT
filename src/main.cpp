174e5527a4ce1af6ae57b2390b2ec1c397c51f24
SebastianMestre <mestresebastianunr@gmail.com>
2019-09-27 23:02:24 -0300

    Add cli argument for target selection

--- src/main.cpp
+++ src/main.cpp
@@ -4,76 +4,130 @@
 
 #include <cstring>
 
+#include "mips_backend.hpp"
 #include "c_backend.hpp"
 #include "frontend.hpp"
 #include "optimizer.hpp"
 
 constexpr char const* usage_string = " -- BrainFuck C Transpiler --\n"
-                                     " -- usage: bfct [-o <name>] <file>\n";
+                                     " -- usage: bfct [-o <name>] [-t <target>] <file>\n"
+                                     " -- target can be one of: c mips\n";
 void bad_usage()
 {
     std::cerr << usage_string;
     std::terminate();
 }
 
+enum class Target {
+	C, MIPS
+};
+
+template<typename Value>
+void report_command_error (int argument_index, char const* expected_description, Value value) {
+	std::cerr << "On argument " << argument_index
+	          << " expected " << expected_description
+	          << " but got: " << value << '\n';
+	bad_usage();
+}
+
 int main(int argc, char** argv)
 {
 
-    if (argc < 2)
-        bad_usage();
+	if (argc < 2)
+		bad_usage();
 
-    std::string output_path{ "out.c" };
-    std::string input_path{};
+	std::string output_path{ "out.c" };
+	std::string input_path{};
+	Target target = Target::C;
 
-    {
-        bool has_input = false;
+	{
+		bool has_input = false;
 
-        for (int i = 1; i < argc; ++i)
-        {
-            if (argv[i][0] == '-')
-            {
-                if (strcmp(argv[i], "-o") == 0)
-                {
-                    output_path = argv[++i];
-                }
-                else
-                {
-                    bad_usage();
-                }
-            }
-            else
-            {
-                if (not has_input)
-                {
-                    input_path = argv[i];
-                    has_input = true;
-                }
-                else
-                {
-                    bad_usage();
-                }
-            }
-        }
+		for (int i = 1; i < argc; ++i)
+		{
+			if (argv[i][0] == '-')
+			{
+				if (strcmp(argv[i], "-o") == 0)
+				{
+					if (i + 1 >= argc)
+					{
+						report_command_error(i + 1, "an output file name", "nothing");
+					}
 
-        if (not has_input)
-            bad_usage();
-    }
+					output_path = argv[++i];
+				}
+				else if (strcmp(argv[i], "-t") == 0)
+				{
+					if (i + 1 >= argc)
+					{
+						report_command_error(i + 1, "a valid target", "nothing");
+					}
 
-    {
-        std::string file_contents;
+					if (strcmp(argv[i + 1], "c") == 0)
+					{
+						target = Target::C;
+					}
+					else if (strcmp(argv[i + 1], "mips") == 0)
+					{
+						target = Target::MIPS;
+					}
+					else
+					{
+						report_command_error(i + 1, "a valid target", argv[i + 1]);
+					}
 
-        {
-            std::ifstream in_file{ input_path };
-            std::string line;
-            while (std::getline(in_file, line))
-            {
-                file_contents.append(line);
-            }
-        }
+					++i;
+				}
+				else
+				{
+					bad_usage();
+				}
+			}
+			else
+			{
+				if (not has_input)
+				{
+					input_path = argv[i];
+					has_input = true;
+				}
+				else
+				{
+					bad_usage();
+				}
+			}
+		}
 
-        std::ofstream out_file{ output_path };
-        out_file << write_c_code(optimize(compile(file_contents)));
-    }
+		if (not has_input)
+		{
+			bad_usage();
+		}
+	}
 
-    return 0;
+	{
+		std::string file_contents;
+
+		{
+			std::ifstream in_file{ input_path };
+			std::string line;
+			while (std::getline(in_file, line))
+			{
+				file_contents.append(line);
+			}
+		}
+
+		std::ofstream out_file{ output_path };
+		auto compilation_result = optimize(compile(file_contents));
+
+		switch (target)
+		{
+		case Target::C:
+			out_file << write_c_code(compilation_result);
+			break;
+		case Target::MIPS:
+			out_file << write_mips_code(compilation_result);
+			break;
+		}
+	}
+
+	return 0;
 }
--- src/mips_backend.cpp
+++ src/mips_backend.cpp
@@ -5,10 +5,12 @@
 
 namespace {
 constexpr char const* output_header = ".data\n"
-                                      "buffer: .space 1000\n"
                                       ".text\n"
                                       "main:\n"
-                                      "la $t0, buffer\n"
+									  "li $v0, 9\n"
+									  "li $a0, 1000\n"
+									  "syscall\n"
+                                      "move $t0, $v0\n"
                                       "addi $t0, $t0, 500\n";
 
 constexpr char const* output_footer = "li $v0, 10\n"
@@ -19,17 +21,6 @@ constexpr char const* output_footer = "li $v0, 10\n"
  * Memory pointer will be $t0
  * We will use $t1 as aux register 1
  * We will use $t2 as aux register 2
-
-            case Opcode::Lop: result << "while(*p){\n"; break;
-            case Opcode::Lcl: result << "}\n"; break;
-
-            case Opcode::Cop: result << "if(*p){\n"; break;
-            case Opcode::Ccl: result << "}\n"; break;
-
-            case Opcode::Put: result << "putchar(*p);\n"; break;
-
-
-
  */
 
 std::string write_mips_code(std::vector<Op> const& code)
@@ -48,17 +39,15 @@ std::string write_mips_code(std::vector<Op> const& code)
         switch (op.opcode)
         {
             case Opcode::Lop: {
-                result << "#Loop Start\n";
                 int loopId = loopCount;
                 loopStack.push_back(loopCount);
                 loopCount++;
                 
                 result << "L" << loopId << ":\n";
-                result << "lb $t1, ($t0)\n";
-                result << "beq $t1, 0, LE" << loopId << "\n";
+                result << "lb $t1, 0($t0)\n";
+                result << "beq $t1, $0, LE" << loopId << "\n";
             } break;
             case Opcode::Lcl: {
-                result << "#Loop End\n";
                 int loopId = loopStack.back();
                 loopStack.pop_back();
                 
@@ -70,14 +59,12 @@ std::string write_mips_code(std::vector<Op> const& code)
             case Opcode::Ccl: result << "}\n"; break;
 
             case Opcode::Put: {
-                result << "#Put\n";
                 result << "li $v0, 11\n";
                 result << "lb $t1, ($t0)\n";
                 result << "move $a0, $t1\n";
                 result << "syscall\n";
             } break;
             case Opcode::Get: {
-                result << "#Get\n";
                 result << "li $v0, 12\n";
                 result << "syscall\n";
                 result << "move $t1, $v0\n";
@@ -86,20 +73,17 @@ std::string write_mips_code(std::vector<Op> const& code)
             } break;
 
             case Opcode::Addi: {
-                result << "#*p += " << int(op.arg1) << ";\n";
                 result << "lb $t1, ($t0)\n";
                 result << "addi $t1, $t1, " << int(op.arg1) << "\n";
                 result << "andi $t1, $t1, 0x00ff\n";
                 result << "sb $t1, ($t0)\n";
             } break;
             case Opcode::Seti: {
-                result << "#*p = getchar();\n";
                 result << "li $t1, " << int(op.arg1) << "\n";
                 result << "andi $t1, $t1, 0x00ff\n";
                 result << "sb $t1, ($t0)\n";
             } break;
             case Opcode::Muli: {
-                result << "#*p *= " << int(op.arg1) << ";\n";
                 result << "lb $t1, ($t0)\n";
                 result << "li $t2, " << int(op.arg1) << "\n";
                 result << "mult $t1, $t2\n";
@@ -109,7 +93,6 @@ std::string write_mips_code(std::vector<Op> const& code)
             } break;
 
             case Opcode::Madd: {
-                result << "#*p += p[" << int(op.arg2) << "] * " << int(op.arg1) << ";\n";
                 result << "lb $t1, " << int(op.arg2) << "($t0)\n";
                 result << "li $t2, " << int(op.arg1) << "\n";
                 result << "mult $t2, $t1\n";
@@ -121,7 +104,6 @@ std::string write_mips_code(std::vector<Op> const& code)
             } break;
 
             case Opcode::Jmp: {
-            	result << "#p += " << int(op.arg1) << ";\n";
                 result << "addi $t0, $t0, " << int(op.arg1) << "\n";
             } break;
 
